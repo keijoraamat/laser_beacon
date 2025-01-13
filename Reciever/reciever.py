007d105e157a861f07addc4d08f2d2ef5cb1bd95
keijoraamat <raamatkeijo@gmail.com>
2025-01-13 23:29:40 +0200

    logging handler for reciever

--- Reciever/reciever.py
+++ Reciever/reciever.py
@@ -5,11 +5,25 @@ import binascii
 import struct
 import json
 import time
+import logging
+import logging.config
+import signal
+import os
 
 
 BEACON_NAME = "LB"
 MANUFACTURER_DATA_TYPE = 0x02E5  # Manufacturer Specific Data (AD Type)
 BROKER_LOCATION = "192.168.0.247:9092"
+MESSAGES_TOPIC = "beacon_data"
+LOG_CONFIG_FILE = "logging_config.ini"
+
+if os.path.exists(LOG_CONFIG_FILE):
+    logging.config.fileConfig(LOG_CONFIG_FILE)
+else:
+    logging.basicConfig(level=logging.DEBUG)
+    logging.warning(f"Logging configuration file '{LOG_CONFIG_FILE}' not found. Using basic configuration.")
+
+logger = logging.getLogger("reciever_logger")
 
 last_timestamp = None
 
@@ -26,33 +40,35 @@ class KafkaHandler:
             self.producer.produce(self.topic, value=json.dumps(data).encode('utf-8'))
             self.producer.flush()
         except Exception as e:
-            print(f"Failed to send data to Kafka: {e}")
+            logger.error(f"Failed to send data to Kafka: {e}")
 
-kafka_handler = KafkaHandler(broker=BROKER_LOCATION, topic="beacon_data")
-
+kafka_handler = KafkaHandler(broker=BROKER_LOCATION, topic=MESSAGES_TOPIC)
 
 async def beacon_callback(device, advertisement_data):
     global last_timestamp
 
     if advertisement_data.local_name == BEACON_NAME:
 
-        #print(f"ad_data: {advertisement_data}")
+        logger.debug(f"found beacon named: {BEACON_NAME}")
+
+        logger.debug(f"ad_data: {advertisement_data}")
         #for key, value in advertisement_data.manufacturer_data.items():
-        #    print(f"Manufacturer data key: {hex(key)}, value: {binascii.hexlify(value)}")
+        #    logger.debug(f"Manufacturer data key: {hex(key)}, value: {binascii.hexlify(value)}")
         manufacturer_data = next(iter(advertisement_data.manufacturer_data.values()), None)
 
         if manufacturer_data:
             # Add leading two zero bytes
             if len(manufacturer_data) == 10:
                 manufacturer_data = b'\x00\x00' + manufacturer_data
-            #print(f"Raw Manufacturer_data (hex): {binascii.hexlify(manufacturer_data)}")
-            # Extract the data from the advertisement
+            logger.debug(f"Raw Manufacturer_data (hex): {binascii.hexlify(manufacturer_data)}")
+
             try:
                 timestamp, number1, number2 = struct.unpack(
                     '>I3s3s', manufacturer_data[:10]
                 )
                 number1 = int.from_bytes(number1, byteorder='big') / 1000.0
                 number2 = int.from_bytes(number2, byteorder='big') / 1000.0
+                logger.debug(f"n1: {number1}, n2: {number2}")
 
                 if last_timestamp is None or timestamp != last_timestamp:
                     last_timestamp = timestamp
@@ -64,22 +80,35 @@ async def beacon_callback(device, advertisement_data):
                         "y": number2
                     }
 
+                    logger.debug(f"data from {device.address} to kafka: {data}")
                     kafka_handler.send_data(data)
 
             except struct.error as e:
-                print(f"Failed to unpack manufacturer data: {e}")
+                logger.error(f"Failed to unpack manufacturer data: {e}")
 
 async def scan_beacon():
     scanner = BleakScanner()
     scanner.register_detection_callback(beacon_callback)
-    print("Scanning for BLE beacons...")
+    logger.info("Scanning for BLE beacons...")
     await scanner.start()
     try:
         while True:
             await asyncio.sleep(1)
     except asyncio.CancelledError:
-        print("Stopping scanner...")
+        logger.info("Stopping scanner...")
         await scanner.stop()
 
+async def main():
+    task = asyncio.create_task(scan_beacon())
+    try:
+        await task
+    except KeyboardInterrupt:
+        logger.info("KeyboardInterrupt detected. Stopping...")
+        task.cancel()
+        try:
+            await task
+        except asyncio.CancelledError:
+            logger.info("Scanner stopped.")
+
 if __name__ == "__main__":
-    asyncio.run(scan_beacon())
+    asyncio.run(main())
